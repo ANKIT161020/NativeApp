3a366b902e3d449246e33d2b545696e30f52f9c5
ANKIT161020 <aab2@somaiya.edu>
2024-03-17 17:25:34 +0530

    removed unnecessary code and added splash image

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet, Text, View } from "react-native";
 import Signup from "./Screens/Signup";
 import Login from "./Screens/Login";
 import Home from "./Screens/Home";
-import OpeningPage from "./Screens/OpeningPage";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,10 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Opening"
+        initialRouteName="Signup"
         screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen name="Opening" component={OpeningPage} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
@@ -24,12 +21,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
